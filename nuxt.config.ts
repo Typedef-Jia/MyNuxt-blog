958af0a5e3ad92d4492b70d6a66afcb8e42c8c57
main中世界 <163827386+Typedef-Joy@users.noreply.github.com>
2024-12-27 08:34:38 +0000

    语言

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+      htmlAttrs: {
+        lang: 'zh'
+      }
+    }
   },
   devtools: { enabled: true },
   modules: ["@nuxt/content", "nuxt-svgo", "@nuxt/image", "@nuxtjs/tailwindcss"],
